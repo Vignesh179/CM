afcab1b708e74e3d157c3d041c997e144f339de1
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-04-07 18:18:04 +0530

    Commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit, Input, SecurityContext } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
-
+  imagesrc:any;
+  constructor(private sanitizer: DomSanitizer) {}
+  imagee(){
+    this.sanitizer.sanitize(SecurityContext.STYLE, 'url(' + this.imagesrc + ')');
+  }
 }
