d1cedc674aa706eb37ab2ad9ce5a4b849d732169
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-04-09 17:30:00 +0530

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Category } from './Category'; 
+import { Category } from './Category';
 @Injectable({
   providedIn: 'root'
 })
@@ -17,11 +17,9 @@ export class ApiService {
     }
   }
   //Insert
-  async post(url: string, data?: any) 
-  {
-    try 
-    {
-      const resp = await this.httpClient.post(url, data, { headers: {},}).toPromise();
+  async post(url: string, data?: any) {
+    try {
+      const resp = await this.httpClient.post(url, data, { headers: {}, }).toPromise();
       return resp;
     } catch (e) {
       return null;
@@ -29,15 +27,14 @@ export class ApiService {
   }
   //Delete
   deleteCategory(id: number) {
-		return this.httpClient.delete<Category>(`${this.LOCALHOST}/delete_category.php/?id=${id}`);
-	}
+    return this.httpClient.delete<Category>(`${this.LOCALHOST}/delete_category.php/?id=${id}`);
+  }
   //Update
-  async put(url: string, data?: any) 
-  {
-    try 
-    {
-      const resp = await this.httpClient.put(url, data, { headers: {},
-       }).toPromise();
+  async put(url: string, data?: any) {
+    try {
+      const resp = await this.httpClient.put(url, data, {
+        headers: {},
+      }).toPromise();
       return resp;
     } catch (e) {
       return null;
