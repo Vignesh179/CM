0e9f1bcc8fe152b8d17da12db2e48b79f43d41e3
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-04-09 17:29:14 +0530

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,29 +1,27 @@
 import { DomSanitizer } from '@angular/platform-browser';
 import { Component, OnInit, Input, SecurityContext } from '@angular/core';
-
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-  imagesrc:any;
-  img1:any;
-  constructor(private sanitizer: DomSanitizer) {}
-  imagee(){
+  imagesrc: any;
+  img1: any;
+  constructor(private sanitizer: DomSanitizer) { }
+  imagee() {
     this.sanitizer.sanitize(SecurityContext.STYLE, 'url(' + this.imagesrc + ')');
   }
   fileChange(event) {
     if (event.target.files && event.target.files[0]) {
       let reader = new FileReader();
-      reader.onload = (event:any) => {
+      reader.onload = (event: any) => {
         this.img1 = event.target.result;
       }
       reader.readAsDataURL(event.target.files[0]);
     }
-    
-    let fileList: FileList = event.target.files;  
+    let fileList: FileList = event.target.files;
     let file: File = fileList[0];
     console.log(file);
   }
